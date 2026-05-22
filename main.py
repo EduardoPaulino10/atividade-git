ca535951a3c89731a1a67002823626bc8c949481
Eduardo Paulino <edu.paulino2010@gmail.com>
2026-05-22 10:18:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-print(2 + 2)
+import math
 
-print(10 - 5)
-
-print(3 * 3)
-
-print(100 / 2)
+print(math.sqrt(9))   # raiz de 9 = 3
+print(math.pow(2, 3)) # 2 elevado a 3 = 8
+print(math.ceil(4.2)) # arredonda pra cima = 5
